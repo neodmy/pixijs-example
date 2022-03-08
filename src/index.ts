fae6cc0f864872136f35e3d29458199888847a51
David Yusta <david.yusta@guidesmiths.com>
2022-03-08 19:11:46 +0100

    fix: amend indentation issues

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Sprite } from 'pixi.js';
 
 const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+  view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0x6495ed,
+  width: 640,
+  height: 480,
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+const clampy: Sprite = Sprite.from('clampy.png');
 
 clampy.anchor.set(0.5);
 
