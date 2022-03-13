30fe5345ab8a578c4f8b5e6c8b7ab021552798b0
David Yusta <david.yusta@guidesmiths.com>
2022-03-13 12:52:41 +0100

    refactor: use Manager and load textures

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
-import { Application, Sprite } from 'pixi.js';
+import Manager from './Manager/Manager';
+import LoaderScene from './scenes/LoaderScene';
 
-const app = new Application({
-  view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
-  resolution: window.devicePixelRatio || 1,
-  autoDensity: true,
-  backgroundColor: 0x6495ed,
-  width: 640,
-  height: 480,
-});
-
-const clampy: Sprite = Sprite.from('clampy.png');
-
-clampy.anchor.set(0.5);
-
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
-
-app.stage.addChild(clampy);
+Manager.initialize(640, 480, 0x6495ed);
+const loaderScene: LoaderScene = new LoaderScene();
+Manager.changeScene(loaderScene);
