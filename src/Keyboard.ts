25324fdef25b708f5965724579acfc32a2bdf853
David Yusta <david.yusta@guidesmiths.com>
2022-03-13 12:46:11 +0100

    feat: add Keyboard class

---
+++ src/Keyboard.ts
@@ -0,0 +1,18 @@
+class Keyboard {
+  public static readonly state: Map<string, boolean>;
+
+  public static initialize() {
+    document.addEventListener('keydown', Keyboard.keyDown);
+    document.addEventListener('keyup', Keyboard.keyUp);
+  }
+
+  private static keyDown(e: KeyboardEvent): void {
+    Keyboard.state.set(e.code, true);
+  }
+
+  private static keyUp(e: KeyboardEvent): void {
+    Keyboard.state.set(e.code, false);
+  }
+}
+
+export default Keyboard;
